d77f944f914f665ec846199f3d1cd7445e6f1e92
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-25 23:09:53 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+ 
+public class Main {
+  
+    public static void main(String[] args) {
+        Person person = new Person("jeck spero", "caribim", "32/6/1764",3 );
+ 
+        
+        System.out.println("Name: " + person.name + "\n" + "Nationality: " 
+        + person.nationality + "\n" + "Date of Birth: " + person.dateOfBirth + 
+        "\n" + "Seat Number: " +person.seatNumber + "\n");
+        
+
+    }
+  
+  
+}
